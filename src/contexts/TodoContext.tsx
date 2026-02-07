68e630b986196e35d5f9bca84fdb7e8eb308f8ae
zanadoman <domanseven@gmail.com>
2026-02-07 19:00:45 +0100

    mmkv storage

--- src/contexts/TodoContext.tsx
+++ src/contexts/TodoContext.tsx
@@ -1,5 +1,6 @@
 import { Todo } from "../models/Todo";
 import { createContext, JSX, ReactNode, useContext, useRef, useState } from "react";
+import { Storage } from "../../Globals";
 
 interface TodoContextType {
   todos: Map<number, Todo>;
@@ -8,27 +9,33 @@ interface TodoContextType {
   removeTodo: (id: number) => void;
 }
 
+const key = "todos";
 const TodoContext = createContext<TodoContextType | undefined>(undefined);
 
 export function TodoProvider({ children }: { children: ReactNode }): JSX.Element {
-  const [todos, setTodos] = useState<Map<number, Todo>>(new Map());
+  const [todos, setTodos] = useState<Map<number, Todo>>(
+    new Map(JSON.parse(Storage.getString(key) ?? "[]")),
+  );
   const id = useRef(0);
 
   function addTodo(todo: Todo): void {
     const map = new Map(todos);
     map.set((todo.id = id.current += 1), { ...todo, done: false });
+    Storage.set(key, JSON.stringify([...map]));
     setTodos(map);
   }
 
   function toggleTodo(id: number): void {
     const map = new Map(todos);
     map.get(id)!.done = !map.get(id)!.done;
+    Storage.set(key, JSON.stringify([...map]));
     setTodos(map);
   }
 
   function removeTodo(id: number): void {
     const map = new Map(todos);
     map.delete(id);
+    Storage.set(key, JSON.stringify([...map]));
     setTodos(map);
   }
 
