f3bd8cde731983b1af14e84e18cd2c748de557cd
zanadoman <domanseven@gmail.com>
2026-02-08 14:02:16 +0100

    show qr code

--- src/widgets/TodoWidget.tsx
+++ src/widgets/TodoWidget.tsx
@@ -3,32 +3,40 @@ import { JSX, useState } from "react";
 import { useTodos } from "../contexts/TodoContext";
 import { View } from "react-native";
 import { Avatar, Button, Card, Dialog, IconButton, Portal, Text } from "react-native-paper";
+import QRCode from "react-native-qrcode-svg";
 
 export function TodoWidget({ todo }: { todo: Todo }): JSX.Element {
-  const [dialog, setDialog] = useState(false);
+  const [qrcode, showQrcode] = useState(false);
+  const [remove, showRemove] = useState(false);
   const { toggleTodo, removeTodo } = useTodos();
 
   return (
     <View>
-      <Card>
+      <Card onPress={() => showQrcode(true)}>
         <Card.Title
           title={todo.title}
           subtitle={todo.content}
           left={(props) => <Avatar.Icon {...props} icon="rocket" />}
         />
         <Card.Actions>
-          <IconButton icon="delete" onPress={() => setDialog(true)} />
+          <IconButton icon="delete" onPress={() => showRemove(true)} />
           <IconButton icon="check" onPress={() => toggleTodo(todo.id)} />
         </Card.Actions>
       </Card>
       <Portal>
-        <Dialog visible={dialog} onDismiss={() => setDialog(false)}>
+        <Dialog visible={qrcode} onDismiss={() => showQrcode(false)}>
+          <Dialog.Title style={{ textAlign: "center" }}>{todo.title}</Dialog.Title>
+          <Dialog.Content style={{ alignItems: "center" }}>
+            <QRCode value={JSON.stringify(todo)} size={200} backgroundColor="transparent" />
+          </Dialog.Content>
+        </Dialog>
+        <Dialog visible={remove} onDismiss={() => showRemove(false)}>
           <Dialog.Title>Confirm</Dialog.Title>
           <Dialog.Content>
             <Text variant="bodyMedium">Are you sure you want to delete?</Text>
           </Dialog.Content>
           <Dialog.Actions>
-            <Button onPress={() => setDialog(false)}>Cancel</Button>
+            <Button onPress={() => showRemove(false)}>Cancel</Button>
             <Button onPress={() => removeTodo(todo.id)}>Delete</Button>
           </Dialog.Actions>
         </Dialog>
